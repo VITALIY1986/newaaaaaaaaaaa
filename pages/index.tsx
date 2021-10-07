a5de6f69ec9c10036b41d8b81c44f1917ebdeb23
VITALIY1986 <staslego1986@gmail.com>
2021-10-07 17:49:24 +0300

    asd

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,12 +38,9 @@ const Index = (props: IIndexProps) => {
               <img className="md:h-screen md:object-cover bg_small object-cover w-full pt-16" src="/static/Bgsmall.jpg" alt=""  />
             </div>
           <div className="text-center md:hidden ">
-   <h1 className="text-white uppercase text-2xl leading-10 mt-8">in caviar
-we trust</h1>
+  
       <div className="flex justify-center mt-4 mb-10 ease-linea  ">
-        <img className="mr-1.5 w-1.5" src="/static/image 2.svg" alt=""  />
-        <img className="mr-1.5 mt-2 w-1.5" src="/static/image 3.svg" alt="" />
-        <img className="w-1.5"src="/static/image 6.svg" alt=""/>
+   
       </div ></div ></div>
           </section>
       
